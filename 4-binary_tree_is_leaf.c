3db4a50a593b1abd883f9f0c2f80f460893648be
SAAJEVES <eveshofase@gmail.com>
2023-11-29 03:20:45 +0100

    4.1

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	int val;
 
 	val = 0;
-	if (node->right == NULL && node->left == NULL)
+	if (node->right == NULL && node->left == NULL && node->parent != NULL)
 	{
 		val++;
 		return (val);
