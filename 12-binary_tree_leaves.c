7b8e8819c67a832d1183efc5ab90240000dec48a
SAAJEVES <eveshofase@gmail.com>
2023-11-30 01:28:53 +0100

    12.1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	num_leaves = 0;
 	if (tree == NULL)
+	{
 		return (0);
+	}
 	else
 	{
 		num_leaves += (!tree->left && !tree->right) ? 1 : 0;
